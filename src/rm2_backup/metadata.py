a793444b18c96a62557f6d9c3d6bdacd09b2e929
Sharif <sharif.iftekhar@gmail.com>
2026-06-24 17:52:30 +0100

    Add metadata module skeleton

---
+++ src/rm2_backup/metadata.py
@@ -0,0 +1,7 @@
+"""RM2 metadata parsing boundaries.
+
+Planned responsibilities:
+- parse local copies of UUID metadata/content files;
+- extract visible names, parent UUIDs, document types and deleted/trash state;
+- avoid assuming that raw filesystem layout equals visible RM2 folder layout.
+"""
