a69005f4d1ef398df23d855feeeecd129eb3e845
Sharif <sharif.iftekhar@gmail.com>
2026-06-24 17:53:46 +0100

    Add publication module skeleton

---
+++ src/rm2_backup/publish.py
@@ -0,0 +1,7 @@
+"""Atomic PDF mirror publication boundaries.
+
+Planned responsibilities:
+- publish validated PDFs from staging to pdf/current;
+- avoid partial or failed outputs appearing in the mirror;
+- apply conservative handling of moved, renamed, deleted and trashed items.
+"""
